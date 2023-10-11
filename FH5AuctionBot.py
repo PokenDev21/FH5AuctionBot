8c95ce40698168612a1cc3142e0ca1afff62173e
PokenDev21 <110134664+PokenDev21@users.noreply.github.com>
2023-10-11 18:18:44 +0200

    Added a resolution toggle and made ui updates

--- FH5AuctionBot.py
+++ FH5AuctionBot.py
@@ -8,10 +8,14 @@ from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 import threading
 
-# ... (Your pixel checking and key simulation functions)
 
 # Define a flag variable to control script execution
 running_flag = False
+
+# Initialize reference resolution and multipliers
+reference_resolution = (1920, 1080)
+x_multiplier = 1.0
+y_multiplier = 1.0
 
 # Function to check if a pixel is within the specified RGB range
 def is_white(pixel):
@@ -23,21 +27,37 @@ def is_purple(pixel):
 # Define a flag variable to control script termination
 stop_flag = False
 # Function to start the script
+
 def start_script():
-    global running_flag
+    global TimeRem_pixel_coords, CarFound_pixel_coords, No_cars_available_pixel_coords, running_flag, x_multiplier,  y_multiplier
+    TimeRem_pixel_coords = (295 , 370)
+    CarFound_pixel_coords = (510, 297)
+    No_cars_available_pixel_coords = (1295 , 557)
+    chosen_resolution = resolution_var.get()
+    chosen_width, chosen_height = map(int, chosen_resolution.split("x"))
+    x_multiplier = chosen_width / reference_resolution[0]
+    y_multiplier = chosen_height / reference_resolution[1]
+    # Multiply each coordinate by the multipliers
+    TimeRem_pixel_coords = (TimeRem_pixel_coords[0] * x_multiplier, TimeRem_pixel_coords[1] * y_multiplier)
+    CarFound_pixel_coords = (CarFound_pixel_coords[0] * x_multiplier, CarFound_pixel_coords[1] * y_multiplier)
+    No_cars_available_pixel_coords = (No_cars_available_pixel_coords[0] * x_multiplier, No_cars_available_pixel_coords[1] * y_multiplier)
+
+
     if not running_flag:
         running_flag = True
         start_button.config(state=tk.DISABLED)
-        stop_button.config(state=tk.NORMAL)
+        resolution_dropdown.config(state=tk.DISABLED)
         log_text.config(state=tk.NORMAL)
-        log_text.insert(tk.END, "Script has been startedy.\n")
-        run_script()  # Start the script when "Begin" is clicked
-
+        log_text.insert(tk.END, "Starting script for resolution in 5 sec\n")
+        log_text.insert(tk.END, f"Set resolution: {chosen_resolution}\n")
+        log_text.insert(tk.END, f"Multiplier X: {x_multiplier} multiplier Y: {y_multiplier}\n")
+        root.after(5000, run_script)  # Start the script after 5 seconds
 # Function to stop the script
 def stop_script():
     global running_flag
     running_flag = False
     start_button.config(state=tk.NORMAL)
+    resolution_dropdown.config(state=tk.NORMAL)
     stop_button.config(state=tk.DISABLED)
     log_text.insert(tk.END, "Script has been stopped.\n")
     # Function to stop the script
@@ -47,6 +67,8 @@ def Reset_Console():
 # Function to run the script in a separate thread
 def run_script():
     def script_runner():
+        stop_button.config(state=tk.NORMAL)
+        root.update()  # Update the GUI to display the message
         while running_flag:
             if simulate_key_presses():
                 stop_script()
@@ -77,14 +99,22 @@ start_button.pack(pady=10)
 stop_button = ttk.Button(root, text="Stop", command=stop_script, state=tk.DISABLED)
 stop_button.pack(pady=10)
 
-# Create a stop button
-Reset_button = ttk.Button(root, text="Reset Console", command=Reset_Console)
-Reset_button.pack(pady=10)
-
+# Create a resolution dropdown with options
+resolution_label = ttk.Label(root, text="Resolution:")
+resolution_label.pack()
+resolution_options = ["1920x1080", "2560x1440", "3840x2160"]
+resolution_var = tk.StringVar(value=resolution_options[0])
+resolution_dropdown = ttk.Combobox(root, textvariable=resolution_var, values=resolution_options)
+resolution_dropdown.pack(pady=10)
 
 # Create a text area to display logs
 log_text = ScrolledText(root, height=10, width=50, wrap=tk.WORD, state=tk.DISABLED)
 log_text.pack(padx=10, pady=10)
+
+# Create a stop button
+Reset_button = ttk.Button(root, text="Clear Console", command=Reset_Console)
+Reset_button.pack(pady=10)
+
 
 root.protocol("WM_DELETE_WINDOW", on_closing)
 
@@ -105,16 +135,16 @@ def simulate_key_presses():
     # Check for a white pixel during the 5-second delay
     start_time = time.time()
     while time.time() - start_time <  2:  # Check for 5 seconds
-        pixel_coords = (510, 297)
-        screenshot = ImageGrab.grab(bbox=(pixel_coords[0], pixel_coords[1], pixel_coords[0] + 1, pixel_coords[1] + 1))
+        screenshot = ImageGrab.grab(bbox=(CarFound_pixel_coords[0], CarFound_pixel_coords[1], CarFound_pixel_coords[0] + 1, CarFound_pixel_coords[1] + 1))
         pixel_color = screenshot.getpixel((0, 0))
         if is_white(pixel_color):
+            if not WhitePixelFound:
+                log_text.insert(tk.END, "Car Found!\n")
             WhitePixelFound = True      
-            pixel_coords = (295 , 370 )
-            screenshot = ImageGrab.grab(bbox=(pixel_coords[0], pixel_coords[1], pixel_coords[0] + 1, pixel_coords[1] + 1))
+            screenshot = ImageGrab.grab(bbox=(TimeRem_pixel_coords[0], TimeRem_pixel_coords[1], TimeRem_pixel_coords[0] + 1, TimeRem_pixel_coords[1] + 1))
             pixel_color = screenshot.getpixel((0, 0))
             if is_purple(pixel_color):
-                print("InstantBuy")
+                log_text.insert(tk.END, "Attempting Buyout\n")
                 # Simulate pressing 'Y' and then 'Enter' keys
                 pyautogui.press('y')
                 time.sleep(random.uniform(0.15,0.16))
@@ -127,8 +157,7 @@ def simulate_key_presses():
                 return True  # Return True if white pixel is found'''
        
        
-        pixel_coords = (1295 , 557)
-        screenshot = ImageGrab.grab(bbox=(pixel_coords[0], pixel_coords[1], pixel_coords[0] + 1, pixel_coords[1] + 1))
+        screenshot = ImageGrab.grab(bbox=(No_cars_available_pixel_coords[0], No_cars_available_pixel_coords[1],No_cars_available_pixel_coords[0] + 1, No_cars_available_pixel_coords[1] + 1))
         pixel_color = screenshot.getpixel((0, 0))
         if is_white(pixel_color):
             pyautogui.press('esc')
